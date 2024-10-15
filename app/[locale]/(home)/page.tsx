4756eead9c91414ebf25380d901c16ffac9c7562
ARYAN TYAGI <aryantyagi111s@gmail.com>
2024-10-15 12:58:22 +0530

    added btn

--- app/[locale]/(home)/page.tsx
+++ app/[locale]/(home)/page.tsx
@@ -46,7 +46,7 @@ export default function HomePage() {
                         <div className="bg-white overflow-hidden rounded-2xl shadow-lg max-w-4xl 3xl:max-w-6xl w-full relative">
                             <button
                                 onClick={() => setIsVisible(false)}
-                                className="absolute top-2 right-2 text-gray-500 hover:text-gray-700"
+                                className="absolute z-[50] top-2 right-2 text-gray-500 hover:text-gray-700"
                             >
                                 <X className="h-6 w-6" />
                             </button>
@@ -61,8 +61,8 @@ export default function HomePage() {
 
                                     />
                                 </div>
-                                <div className="lg:w-3/4 w-full flex items-center px-4 flex-col justify-center lg:px-8">
-                                    <h2 className="lg:text-4xl text-lg font-bold mb-4">{
+                                <div className="lg:w-3/4 w-full flex  px-4 flex-col justify-center lg:px-8">
+                                    <h2 className="lg:text-4xl text-lg font-bold text-left mb-4">{
                                         languageData?.popup?.[
                                             changeLanguage
                                         ]?.Heading
