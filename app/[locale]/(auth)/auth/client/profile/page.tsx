6082a303d351c1526b40755b735a8e96154c9b15
ARYAN TYAGI <aryantyagi111s@gmail.com>
2024-10-20 13:52:54 +0530

    fixing build errors

--- app/[locale]/(auth)/auth/client/profile/page.tsx
+++ app/[locale]/(auth)/auth/client/profile/page.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Shield } from "lucide-react"
